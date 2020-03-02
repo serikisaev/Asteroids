3669566307d9cdf66d65a9178b97a98a7e3ceb9c
serikisaev <42213906+serikisaev@users.noreply.github.com>
2020-03-02 19:01:11 +0500

    Разделил астероиды на две группы, добавил событие столкновения

--- asteroids_game.py
+++ asteroids_game.py
@@ -11,14 +11,20 @@ def run_game():
     pygame.display.set_caption('Androids Vector')
     ship = Ship(ai_settings, screen)
     bullets = Group()
-    asteroids = pygame.sprite.Group()
-    gf.create_pound(ai_settings, screen, asteroids)
+    asteroids1 = pygame.sprite.Group()
+    asteroids2 = pygame.sprite.Group()
+    gf.create_pound(ai_settings, screen, asteroids1)
+    gf.create_pound(ai_settings, screen, asteroids2)
     running = True
     while running:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         bullets.update()
-        asteroids.update()
-        gf.update_screen(ai_settings, screen, ship, asteroids, bullets)
+        asteroids1.update()
+        gf.update_screen(ai_settings, screen, ship, asteroids1, bullets)
+        asteroids2.update()
+        if pygame.sprite.groupcollide(asteroids1, asteroids2, False, False):
+            print('stolk')
+        gf.update_screen(ai_settings, screen, ship, asteroids2, bullets)
 
 run_game()
